e78727e87d3b75b22909de9ec2d4df35525f802f
Lee Spitzley <lspitzley@users.noreply.github.com>
2026-02-02 17:49:45 +0000

    added a script to show printing

---
+++ class3-1.py
@@ -0,0 +1,22 @@
+"""
+This script will show basic printing functions
+in Python.
+
+To run the script, in the terminal, type:
+python class3-1.py
+"""
+
+# Show a basic print statement
+print("Hello, World!")
+
+# create a variable with your name
+name = "Lee"
+
+# print the name
+print(name)    # only prints name
+
+# print with formatting
+print("Hello,", name)
+
+# print with an 'f' string
+print(f'Hello, {name}!')
