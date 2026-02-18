98b5f71e26bd630e9a19f40a818e7d17de84847e
Lee Spitzley <lspitzley@users.noreply.github.com>
2026-02-18 18:03:57 +0000

    setup for Monday

--- class_testing.py
+++ class_testing.py
@@ -18,3 +18,15 @@ def string_checker(input_string:str, substring:str) -> bool:
         return True
     else:
         return False
+
+def validate_conversion_type(conversion_type:str) -> bool:
+    """
+    Docstring for validate_conversion_type
+    
+    :param conversion_type: Description
+    :type conversion_type: str
+    :return: Description
+    :rtype: bool
+    """
+
+    pass
