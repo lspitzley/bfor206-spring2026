97feb6e89e24a6da8782ef4ca39bb63d1db4e7ff
Lee Spitzley <lspitzley@users.noreply.github.com>
2026-02-18 17:43:31 +0000

    test script runs, but tests fail

---
+++ class_testing.py
@@ -0,0 +1,17 @@
+"""
+A few example functions to demonstrate 
+unit tests.
+"""
+
+def string_checker(input_string:str, substring:str) -> bool:
+    """
+    Check for a substring in an input string.
+    
+    :param input_string: longer string to check
+    :type input_string: str
+    :param substring: substring to check
+    :return: True of False if the substring is found.
+    :rtype: bool
+    """
+
+    pass # placeholder for preventing errors when there is no code yet.
---
+++ tests/test_class_testing.py
@@ -0,0 +1,20 @@
+"""
+Functions to demonstrate testing.
+
+1. run as python -m tests.test_class_testing
+2. install pytest: pip install pytest
+3. run tests: python -m pytest
+"""
+
+# imports
+import class_testing
+
+
+def test_string_checker():
+    """
+    Tests for the substring checker
+    """
+
+    assert class_testing.string_checker('and', 'e') is False
+
+
